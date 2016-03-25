678b2b5838b45bac2014180340bdcb9552ea0db5
Nico De Witte <nico.dewitte@vives.be>
2016-03-25 10:09:53 +0100

    Fix case where not sleeping for 5 seconds could destroy the moon

--- hello.rb
+++ hello.rb
@@ -2,3 +2,10 @@ puts "Hello World from Ruby"
 
 puts "Creating temporary file which should not be included in git repo"
 File.new "./.tmpfile", "w"
+
+puts "Do not push CTRL-c before this sleep end or you will blow up the Moon !!!!"
+
+for i in 0..4
+   puts "Not blowing up the Moon"
+   sleep 1
+end
